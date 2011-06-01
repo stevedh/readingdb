3bae5613525ac085074b83f1c9b8657c00bc725b
stevedh <stevedh@16e81406-0b52-4fc8-9ac4-c3f00167659a>
2011-06-01 15:42:27 +0000

    - fix iface makefile, setup to point to c6, change version to match
    git-svn-id: svn+ssh://smote/code/powerdb/readingdb@5222 16e81406-0b52-4fc8-9ac4-c3f00167659a

--- iface_bin/setup.py
+++ iface_bin/setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup, Extension
 
 readingdb_module = Extension('_readingdb',
                              sources=['readingdb_wrap.c', 'readingdb.c',
-                                      '../c/pbuf/rdb.pb-c.c', '../c/rpc.c'],
+                                      '../c6/pbuf/rdb.pb-c.c', '../c6/rpc.c'],
                              libraries=['protobuf-c'])
 
 setup (name='readingdb',
-       version='5.0',
+       version='0.6',
        author='Stephen Dawson-Haggerty',
        ext_modules=[readingdb_module],
        py_modules='readingdb',)
