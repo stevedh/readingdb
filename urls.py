36a0eb7a19f1d60a71387284ebc91af3a501aac0
Stephen Dawson-Haggerty <stevedh@eecs.berkeley.edu>
2012-01-05 14:51:59 -0800

    remove files

--- urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.conf.urls.defaults import *
-urlpatterns = patterns('',
-  ('^avail/(\d+)', 'powerdb.readingdb.views.availability'),
-  ('^n/(?P<streamid>\d+)', 'powerdb.readingdb.views.npoints'),
-  ('^n/', 'powerdb.readingdb.views.npoints'),
-  ('^iter/(\d+)/(\d+)/([a-z]+)/(\d+)', 'powerdb.readingdb.views.iterate')
-)
